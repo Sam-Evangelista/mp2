c2e3e69835ec3be376ba2775a2d84b77402b2a4b
samev <samevangelista07@icloud.com>
2025-09-30 12:11:05 -0500

    Deploy pls

--- mp2/src/App.tsx
+++ mp2/src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
+async function getPokemon () {
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+    console.log(response);
+    let name:string = response.data.name;
+    let id:number = response.data.id;
+    console.log(name);
+    console.log(id);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
 function App() {
+  let name, id = getPokemon();
   return (
     <div>
+
       <h1>Hello World!</h1>
     </div>
   );
